33c2a9f5910292d6fdc603a245249e0842a09948
Faisal Fawad <faisalfawad2004@gmail.com>
2023-08-21 23:08:10 -0400

    First commit

---
+++ NoteHighlightAddin/AddIn.cs
@@ -0,0 +1,151 @@
+﻿/*
+ *  Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license.
+ */
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Extensibility;
+using Microsoft.Office.Core;
+using NoteHighlightAddin.Utilities;
+using Application = Microsoft.Office.Interop.OneNote.Application;  // Conflicts with System.Windows.Forms
+using System.Reflection;
+using System.Drawing;
+using Microsoft.Office.Interop.OneNote;
+using System.Text;
+using System.Linq;
+using Helper;
+using System.Threading;
+using System.Web;
+using System.Configuration;
+using System.Globalization;
+
+#pragma warning disable CS3003 // Type is not CLS-compliant
+
+namespace NoteHighlightAddin
+{
+	[ComVisible(true)]
+	[Guid("4C6B0362-F139-417F-9661-3663C268B9E9"), ProgId("NoteHighlight2016.AddIn")]
+
+	public class AddIn : IDTExtensibility2, IRibbonExtensibility
+	{
+		protected Application OneNoteApplication
+		{ get; set; }
+
+        public XNamespace ns;
+
+        public AddIn()
+		{
+		}
+
+		/// <summary>
+		/// Returns the XML in Ribbon.xml so OneNote knows how to render our ribbon
+		/// </summary>
+		/// <param name="RibbonID"></param>
+		/// <returns></returns>
+		public string GetCustomUI(string RibbonID)
+		{
+            return LoadRibbon();
+        }
+
+        private string LoadRibbon()
+        {
+            try
+            {
+                var workingDirectory = Path.Combine(ProcessHelper.GetDirectoryFromPath(Assembly.GetCallingAssembly().Location), "ribbon.xml");
+                string file = File.ReadAllText(workingDirectory);
+                return file;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Exception from Addin.LoadRibbon:" + e.Message);
+                return "";
+            }
+        }
+
+        public void OnAddInsUpdate(ref Array custom)
+		{
+		}
+
+		/// <summary>
+		/// Cleanup
+		/// </summary>
+		/// <param name="custom"></param>
+		public void OnBeginShutdown(ref Array custom)
+		{
+		}
+
+		/// <summary>
+		/// Called upon startup.
+		/// Keeps a reference to the current OneNote application object.
+		/// </summary>
+		/// <param name="application"></param>
+		/// <param name="connectMode"></param>
+		/// <param name="addInInst"></param>
+		/// <param name="custom"></param>
+		public void OnConnection(object Application, ext_ConnectMode ConnectMode, object AddInInst, ref Array custom)
+		{
+			SetOneNoteApplication((Application)Application);
+		}
+
+		public void SetOneNoteApplication(Application application)
+		{
+			OneNoteApplication = application;
+		}
+
+		/// <summary>
+		/// Cleanup
+		/// </summary>
+		/// <param name="RemoveMode"></param>
+		/// <param name="custom"></param>
+		[SuppressMessage("Microsoft.Reliability", "CA2001:AvoidCallingProblematicMethods", MessageId = "System.GC.Collect")]
+		public void OnDisconnection(ext_DisconnectMode RemoveMode, ref Array custom)
+		{
+			OneNoteApplication = null;
+			GC.Collect();
+			GC.WaitForPendingFinalizers();
+		}
+
+		public void OnStartupComplete(ref Array custom)
+		{
+		}
+
+        //public async Task AddInButtonClicked(IRibbonControl control)
+        public void AddInButtonClicked(IRibbonControl control)
+        {
+            MessageBox.Show("This is a demo button!");
+        }
+
+        /// <summary>
+        /// Specified in Ribbon.xml, this method returns the image to display on the ribbon button
+        /// </summary>
+        /// <param name="imageName"></param>
+        /// <returns></returns>
+        public IStream GetImage(string imageName)
+		{
+			MemoryStream imageStream = new MemoryStream();
+            //switch (imageName)
+            //{
+            //    case "CSharp.png":
+            //        Properties.Resources.CSharp.Save(imageStream, ImageFormat.Png);
+            //        break;
+            //    default:
+            //        Properties.Resources.Logo.Save(imageStream, ImageFormat.Png);
+            //        break;
+            //}
+
+            BindingFlags flags = BindingFlags.Static | BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic;
+
+            var b = typeof(Properties.Resources).GetProperty(imageName.Substring(0, imageName.IndexOf('.')), flags).GetValue(null, null) as Bitmap;
+            b.Save(imageStream, ImageFormat.Png);
+
+            return new CCOMStreamWrapper(imageStream);
+		}
+    }
+}
